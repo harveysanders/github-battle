75a81cc45f58ac494739211f5844c4d8db637340
Harvey Sanders <sanders.harvey@gmail.com>
2016-03-10 18:32:24 -0600

    update UI

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -2,6 +2,8 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var styles = require('../styles');
 var Link = require('react-router').Link;
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 function puke (object) {
 	return (
@@ -17,14 +19,13 @@ function ConfirmBattle(props) {
 		: <div className='jumbotron col-sm-12 text-center' style={styles.transparentBg}>
 				<h1>Confirm Players</h1>
 				<div className='col-sm-8 col-sm-offset-2'>
-					<div className='col-sm-6'>
-						<p className='lead'>Player 1</p>
-						{puke(props.playersInfo[0])}
-					</div>
-					<div className='col-sm-6'>
-						<p className='lead'>Player 2</p>
-						{puke(props.playersInfo[1])}
-					</div>
+					<UserDetailsWrapper header='Player One'>
+						<UserDetails info={props.playersInfo[0]} />
+					</UserDetailsWrapper>
+
+					<UserDetailsWrapper header='Player Two'>
+						<UserDetails info={props.playersInfo[1]} />
+					</UserDetailsWrapper>
 				</div>
 				<div className='col-sm-8 col-sm-offset-2'>
 					<div className='col-sm-12' style={styles.space}>
@@ -47,5 +48,6 @@ ConfirmBattle.propTypes = {
 	isLoading: PropTypes.bool.isRequired,
 	playersInfo: PropTypes.array.isRequired,
 	onInitiateBattle: PropTypes.func.isRequired
+
 }
 module.exports = ConfirmBattle;
